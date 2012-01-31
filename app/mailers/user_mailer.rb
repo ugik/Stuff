0fd634cec82e8303256946142485beaaa2185873
ugik <georgek@gmail.com>
2012-01-31 17:22:55 -0500

    cell # handling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ActionMailer::Base
   default :from => "ugikma@gmail.com"
 
-	def registration_confirmation(user)
+	def registration_confirmation(user, email)
 		@user = user
-		mail(:to => "#{user.name} <#{user.email}>", :subject => "Registered")
+		mail(:to => "#{user.name} <#{email}>", :subject => "Registered")
 	end
+
+	def receipt_confirmation(user)
+		@user = user
+		mail(:to => "#{user.name} <#{email}>", :subject => "Received")
+	end
+
 end
 
