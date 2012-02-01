c379f6e63ca880bfb1cbefc0b49d1d79445176e8
ugik <georgek@gmail.com>
2012-01-31 22:26:57 -0500

    cell # handling

--- app/mailers/stuff_mailer.rb
+++ app/mailers/stuff_mailer.rb
@@ -55,7 +55,7 @@ class StuffMailer < ActionMailer::Base
       user.email = @email
       user.avatar = avatar_file unless avatar_file.nil?
 
-      UserMailer.receipt_confirmation(user.name, user.email).deliver unless user.nil?
+      UserMailer.receipt_confirmation(user).deliver unless user.nil?
 
     end
   end
